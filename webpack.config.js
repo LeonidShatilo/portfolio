c9f95f876e1e49f7e4d16bdc894727ba3671a646
Leonid Shatilo <shatiloleonid@gmail.com>
2021-02-05 16:00:03 +0300

    feat: add base configuration

---
+++ webpack.config.js
@@ -0,0 +1,173 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const filename = (ext) => (isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`);
+
+const babelOptions = (preset) => {
+  const opts = {
+    presets: ['@babel/preset-env'],
+    plugins: ['@babel/plugin-proposal-class-properties'],
+  };
+
+  if (preset) {
+    opts.presets.push(preset);
+  }
+
+  return opts;
+};
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all',
+    },
+  };
+
+  if (isProd) {
+    config.minimize = true;
+    config.minimizer = [
+      new OptimizeCssAssetsWebpackPlugin(),
+      new TerserWebpackPlugin(),
+    ];
+  }
+
+  return config;
+};
+
+const jsLoaders = () => {
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: babelOptions(),
+    },
+  ];
+
+  if (isDev) {
+    loaders.push('eslint-loader');
+  }
+
+  return loaders;
+};
+
+const plugins = () => {
+  const basePlugins = [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      template: './index.html',
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: `./src/styles/${filename('css')}`,
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'assets'),
+          to: path.resolve(__dirname, 'dist/assets'),
+          noErrorOnMissing: true
+        },
+      ],
+    }),
+  ];
+
+  return basePlugins;
+}
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: ['@babel/polyfill', './app.js'],
+  output: {
+    filename: `./src/scripts/${filename('js')}`,
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.png'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'assets'),
+      '@images': path.resolve(__dirname, 'assets/images'),
+      '@scripts': path.resolve(__dirname, 'src/scripts'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+    },
+  },
+  devServer: {
+    port: 3000,
+    contentBase: path.resolve(__dirname, 'dist'),
+    hot: true,
+    open: true,
+    inline: true,
+    compress: true,
+    historyApiFallback: true,
+
+  },
+  plugins: plugins(),
+  optimization: optimization(),
+  devtool: isProd ? false : 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.html/,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+            },
+          },
+          'css-loader?url=false',
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader?url=false',
+          'sass-loader?sourceMap',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: jsLoaders(),
+      },
+      {
+        test: /\.(?:svg|png|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: `../assets/images/[ext]/${filename('[ext]')}`,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(?:ttf|woff|woff2|eot)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: `../assets/fonts/${filename('[ext]')}`,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
